ca6ed6a8d3403fcdec5fa99f20077d13c4ab601a
SleepingNext <veroskevin8@gmail.com>
2020-03-26 17:25:42 +0700

    Add order APIs

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"github.com/SleepingNext/api-gateway/helper"
 	userPB "github.com/G0tYou/user-service/proto"
+	"github.com/SleepingNext/api-gateway/helper"
+	orderPB "github.com/SleepingNext/order-service/proto"
+	"log"
+
 	//authPB "github.com/SleepingNext/auth-service/proto"
-	//orderPB "github.com/SleepingNext/order-service/proto"
 	//paymentPB "github.com/SleepingNext/payment-service/proto"
 	productPB "github.com/SleepingNext/product-service/proto"
 	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/web"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -559,8 +560,335 @@ func main() {
 		return
 	})
 
+	// Order APIs
+	s.HandleFunc("/order/index", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call IndexOrders rpc from grpc client
+		res, err := client.IndexOrders(ctx, &orderPB.IndexOrdersRequest{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
+	s.HandleFunc("/order/indexByUserID", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		// Take params from request
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the body
+		var user *orderPB.User
+		err = json.Unmarshal(body, &user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call IndexOrders rpc from grpc client
+		res, err := client.IndexOrdersByUserID(ctx, user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
+	s.HandleFunc("/order/show", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		// Take params from request
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the body
+		var order *orderPB.Order
+		err = json.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call ShowOrder rpc from grpc client
+		res, err := client.ShowOrder(ctx, order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
+	s.HandleFunc("/order/store", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		// Take params from request
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the body
+		var order *orderPB.Order
+		err = json.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call StoreOrder rpc from grpc client
+		res, err := client.StoreOrder(ctx, order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
+	s.HandleFunc("/order/update", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "PUT" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		// Take params from request
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the body
+		var order *orderPB.Order
+		err = json.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call UpdateOrder rpc from grpc client
+		res, err := client.UpdateOrder(ctx, order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
+	s.HandleFunc("/order/destroy", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			http.Error(w, "Unsupported http method", http.StatusBadRequest)
+			return
+		}
+
+		// Take params from request
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Unmarshal the body
+		var order *orderPB.Order
+		err = json.Unmarshal(body, &order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		client := helper.NewOrderClient()
+
+		// Get the authorization header
+		authorizationHeader := r.Header.Get("Authorization")
+		if !strings.Contains(authorizationHeader, "Bearer") {
+			http.Error(w, "Invalid token", http.StatusBadRequest)
+			return
+		}
+		token := strings.Replace(authorizationHeader, "Bearer ", "", -1)
+
+		// Create context with token
+		ctx := metadata.NewContext(context.Background(), map[string]string{
+			"token": token,
+		})
+
+		// Call DestroyOrder rpc from grpc client
+		res, err := client.DestroyOrder(ctx, order)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return			}
+
+		// Marshal the response
+		js, err := json.Marshal(res)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Set the header and write the marshaled response
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		return
+	})
+
 	err := s.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
